709b2019eb09480e6cf19b01abdc3571b1b3c94f
Duarte <pedrohduarte007@gmail.com>
2026-08-13 08:35:06 -0300

    feat: Criado features e suas rotas

--- e-commerce-AgroAgrega/src/app/app.routes.ts
+++ e-commerce-AgroAgrega/src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
+
 import { Home } from './features/home/home';
-import { ProductDetails } from './features/product-details/product-details';
 import { Products } from './features/products/products';
+import { ProductDetails } from './features/product-details/product-details';
 import { Cart } from './features/cart/cart';
+
+import { Login } from './features/auth/login/login';
+import { Register } from './features/auth/register/register';
+import { ForgotPassword } from './features/auth/forgot-password/forgot-password';
+import { ResetPassword } from './features/auth/reset-password/reset-password';
+
 import { Checkout } from './features/checkout/checkout';
+
+import { Orders } from './features/orders/orders/orders';
+import { OrderDetails } from './features/orders/order-details/order-details';
 
 export const routes: Routes = [
   {
@@ -23,7 +33,31 @@ export const routes: Routes = [
     component: Cart,
   },
   {
+    path: 'login',
+    component: Login,
+  },
+  {
+    path: 'register',
+    component: Register,
+  },
+  {
+    path: 'forgot-password',
+    component: ForgotPassword,
+  },
+  {
+    path: 'reset-password',
+    component: ResetPassword,
+  },
+  {
     path: 'checkout',
     component: Checkout,
+  },
+  {
+    path: 'orders',
+    component: Orders,
+  },
+  {
+    path: 'orders/:id',
+    component: OrderDetails,
   },
 ];
