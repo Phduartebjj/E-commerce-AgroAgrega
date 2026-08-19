e4a5f7e8564a2d6f963ad573f0c9538624340ff5
Duarte <pedrohduarte007@gmail.com>
2026-08-19 11:31:15 -0300

    feat: Melhora a eficiência da validação de campo

--- e-commerce-AgroAgrega/src/app/features/checkout/checkout.ts
+++ e-commerce-AgroAgrega/src/app/features/checkout/checkout.ts
@@ -32,6 +32,32 @@ export class CheckoutComponent {
       nameNoSpecialChars,
     ]),
   });
+
+  getErrorMessage(controlName: string): string {
+    const control = this.checkoutForm.get(controlName);
+
+    if (!control?.touched || !control.errors) {
+      return '';
+    }
+
+    if (control.errors['required']) {
+      return 'Campo obrigatório';
+    }
+
+    if (control.errors['minlength']) {
+      return 'Campo muito curto';
+    }
+
+    if (control.errors['numberInvalid']) {
+      return 'Não pode conter números ';
+    }
+
+    if (control.errors['charsInvalid']) {
+      return 'Não pode conter caracteres especiais';
+    }
+
+    return '';
+  }
 }
 
 function nameNoSpecialChars(control: AbstractControl): ValidationErrors | null {
