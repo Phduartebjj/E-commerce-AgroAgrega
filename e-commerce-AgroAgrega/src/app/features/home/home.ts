a8a36823c15159f096307fb072dabfc6c71a801b
Lorran Ribeiro <lorrranrb16@gmail.com>
2026-08-20 02:30:17 -0300

    feat: add benefits component to home page and update imports

--- e-commerce-AgroAgrega/src/app/features/home/home.ts
+++ e-commerce-AgroAgrega/src/app/features/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import {Hero} from './components/hero/hero'
+import { Benefits } from './components/benefits/benefits';
 
 @Component({
   selector: 'app-home',
-  imports: [Hero],
+  imports: [Hero, Benefits],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
