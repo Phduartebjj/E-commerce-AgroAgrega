fc89643c8687b83981aa230836e3f752ba63ceb8
Lorran Ribeiro <lorrranrb16@gmail.com>
2026-08-20 21:45:31 -0300

    feat: Adiciona componente de rodapé com estilização e testes

--- e-commerce-AgroAgrega/src/app/features/home/home.ts
+++ e-commerce-AgroAgrega/src/app/features/home/home.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import {Hero} from './components/hero/hero'
 import { Benefits } from './components/benefits/benefits';
+import { Footer } from '../../shared/components/footer/footer';
 
 
 @Component({
   selector: 'app-home',
-  imports: [Hero, Benefits,],
+  imports: [Hero, Benefits,Footer],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
