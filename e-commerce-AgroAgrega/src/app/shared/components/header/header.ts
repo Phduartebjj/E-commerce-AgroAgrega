4621a76c72a183e32f576c4c424217d0d4cbe844
Igor Dias <dev.diasx@gmail.com>
2026-08-23 03:51:10 -0300

    feat(header): add cart count and logged-in user name

--- e-commerce-AgroAgrega/src/app/shared/components/header/header.ts
+++ e-commerce-AgroAgrega/src/app/shared/components/header/header.ts
@@ -1,6 +1,7 @@
-import { Component, inject } from '@angular/core';
-import{ RouterLink,}  from '@angular/router';
 import { Cart } from '@core/services/cart/cart.service';
+import { Component, inject, Signal, signal } from '@angular/core';
+import { RouterLink,}  from '@angular/router';
+import { Auth } from '@core/services/auth/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -12,5 +13,14 @@ export class Header {
   private cart = inject(Cart);
   
   totalItens =this.cart.totalCartItens;
-}
 
+  private auth = inject(Auth);
+
+  public name: Signal<string> = signal(this.userName()).asReadonly();
+  public loggedIn: Signal<boolean> = signal(this.auth.isLoggedIn()).asReadonly(); 
+
+  private userName(): string{
+    const name = this.auth.getName();
+    return name !== '' ? `Olá, ${name.toUpperCase()}!` : 'Entrar ou Cadastrar';
+  }
+}
