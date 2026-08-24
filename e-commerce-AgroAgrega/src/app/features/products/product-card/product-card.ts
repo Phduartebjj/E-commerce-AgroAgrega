b2f657de8a475ce891c70ba7c2efa3ec86206c35
João Vitor Santos Saraiva <vic0249289@aluno.firjansenaisesi.com.br>
2026-08-24 10:42:47 -0300

    fix: Corrige erro de link

--- e-commerce-AgroAgrega/src/app/features/products/product-card/product-card.ts
+++ e-commerce-AgroAgrega/src/app/features/products/product-card/product-card.ts
@@ -2,9 +2,10 @@ import { Component, input, output } from '@angular/core';
 import { UpperCasePipe } from '@angular/common';
 import { PrecoFormatadoPipe } from '../../../shared/pipes/preco-formatado-pipe';
 import { ProductModel } from '@models/product';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-product-card',
-  imports: [UpperCasePipe, PrecoFormatadoPipe],
+  imports: [UpperCasePipe, PrecoFormatadoPipe, RouterLink],
   templateUrl: './product-card.html',
   styleUrl: './product-card.css',
 })
