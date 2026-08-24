55de48962de27ed8d3bdba6e8f699e8f4b284597
Duarte <pedrohduarte007@gmail.com>
2026-08-24 11:37:11 -0300

    fix: Github pages

--- e-commerce-AgroAgrega/src/app/features/home/components/product-carousel/product-carousel.spec.ts
+++ e-commerce-AgroAgrega/src/app/features/home/components/product-carousel/product-carousel.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 
 import { ProductCarousel } from './product-carousel';
 
@@ -9,6 +10,7 @@ describe('ProductCarousel', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ProductCarousel],
+      providers: [provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ProductCarousel);
