6daa2ebbcf496836717e57d41c755af619c397a4
maoel <pedrohduarte007@gmail.com>
2026-08-27 21:29:22 -0300

    feat: Adiciona funcionalidade de nota fiscal

--- e-commerce-AgroAgrega/src/app/features/orders/order-details/order-details.ts
+++ e-commerce-AgroAgrega/src/app/features/orders/order-details/order-details.ts
@@ -4,25 +4,35 @@ import { ActivatedRoute, RouterLink } from '@angular/router';
 import { OrderService } from '@core/services/order/order.service';
 import { PrecoFormatadoPipe } from '@shared/pipes/preco-formatado-pipe';
 import { OrderDetailsCard } from './order-details-card/order-details-card';
+import { ReceiptService } from '@core/services/receipt/receipt';
 
 @Component({
   selector: 'app-order-details',
-  imports: [RouterLink, DatePipe, PrecoFormatadoPipe, OrderDetailsCard],
-
-
+  imports: [DatePipe, PrecoFormatadoPipe, OrderDetailsCard],
   templateUrl: './order-details.html',
   styleUrl: './order-details.css',
 })
 export class OrderDetails {
+  receiptService = inject(ReceiptService);
+
+  generateReceipt() {
+    const order = this.order();
+
+    if (!order) {
+      return;
+    }
+
+    this.receiptService.generateReceipt(order);
+  }
+
   private readonly route = inject(ActivatedRoute);
 
   private readonly OrderService = inject(OrderService);
 
   readonly orderId = this.route.snapshot.paramMap.get('id');
 
-  
   readonly order = computed(() => {
-    if(!this.orderId) {
+    if (!this.orderId) {
       return undefined;
     }
 
