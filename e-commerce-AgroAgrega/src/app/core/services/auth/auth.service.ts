6e9783efba40a5ab484e33223d423cc9a15f5db3
maoel <pedrohduarte007@gmail.com>
2026-08-27 21:53:40 -0300

    feat(Auth): Muda fonte da verdade do auth para um signal

--- e-commerce-AgroAgrega/src/app/core/services/auth/auth.service.ts
+++ e-commerce-AgroAgrega/src/app/core/services/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, inject } from '@angular/core';
+import { Injectable, inject, signal } from '@angular/core';
 import { StorageService } from './storage.service';
 import { TokenAuth } from './token.service';
 import * as crypt from 'crypto-js';
@@ -10,28 +10,35 @@ import { ServiceResponse } from '@models/serviceResponse';
 export class Auth {
   private Storage = inject(StorageService);
   private Token = inject(TokenAuth);
-
-  public isLoggedIn(): boolean{
+  readonly currentUserId = signal<string | null>(null);
+  public isLoggedIn(): boolean {
     return this.Token.checkToken();
   }
 
-  login(email: string, password: string): boolean{
-    const user = this.Storage.getUser(email); 
-    if(!user) return false;
+  private getInitialUserId(): string | null {
+    const id = this.Token.getId();
+    return id || null;
+  }
+
+  login(email: string, password: string): boolean {
+    const user = this.Storage.getUser(email);
+    if (!user) return false;
 
     password = crypt.SHA256(password).toString();
-    if(user.password !== password) return false
-    
+    if (user.password !== password) return false;
+
     this.Token.setToken({
       id: user.id,
       name: user.name,
-      email: user.email
+      email: user.email,
     });
-       
+
+    this.currentUserId.set(user.id);
+
     return true;
   }
 
-  register(name: string, email: string, password: string): ServiceResponse{
+  register(name: string, email: string, password: string): ServiceResponse {
     password = crypt.SHA256(password).toString();
     const id = crypto.randomUUID();
 
@@ -39,45 +46,48 @@ export class Auth {
       id,
       name,
       email,
-      password
+      password,
     });
 
-    if(!user.res) return {res: user.res, message: user.message};
+    if (!user.res) return { res: user.res, message: user.message };
 
     this.Token.setToken({
       id,
       name,
-      email
+      email,
     });
 
-    return {res: user.res, message: user.message};
+    this.currentUserId.set(id);
+
+    return { res: user.res, message: user.message };
   }
-    
-  resetPassword(email: string, newPassword: string): ServiceResponse{
+
+  resetPassword(email: string, newPassword: string): ServiceResponse {
     newPassword = crypt.SHA256(newPassword).toString();
     const resetPassword = this.Storage.updatePasswordUser(email, newPassword);
-    
+
     return resetPassword;
   }
-    
-  updateEmail(newEmail: string): ServiceResponse{
+
+  updateEmail(newEmail: string): ServiceResponse {
     const response = this.Storage.updateEmailUser(this.getId(), newEmail);
 
     return response;
   }
 
-  deleteAccount(id: string){
+  deleteAccount(id: string) {
     this.Storage.removeUser(id);
   }
 
-  logout(){
+  logout() {
     this.Token.deleteToken();
+    this.currentUserId.set(null);
   }
 
-  getId(): string{
+  getId(): string {
     return this.Token.getId();
   }
-  getName(): string{
+  getName(): string {
     return this.Token.getName();
   }
 }
