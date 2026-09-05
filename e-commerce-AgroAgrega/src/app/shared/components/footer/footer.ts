848143e6d29b027a9bb2145150323b7d144b4cdb
Lorran Ribeiro <lorrranrb16@gmail.com>
2026-09-05 14:13:47 -0300

    feat: Aprimora componente de rodapé com detecção de rolagem para visibilidade do botão "voltar ao topo"

--- e-commerce-AgroAgrega/src/app/shared/components/footer/footer.ts
+++ e-commerce-AgroAgrega/src/app/shared/components/footer/footer.ts
@@ -1,17 +1,47 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  HostListener,
+  signal,
+  ViewChild,
+} from '@angular/core';
+
 import { RouterLink } from '@angular/router';
+
 @Component({
   selector: 'app-footer',
   imports: [RouterLink],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
-  
 })
-export class Footer {
+export class Footer implements AfterViewInit {
+  mostrarBotaoVoltarAoTopo = signal(false);
+
+  @ViewChild('footerRoot')
+  footer!: ElementRef<HTMLElement>;
+
+  ngAfterViewInit(): void {
+    this.verificarFooter();
+  }
+
+  @HostListener('window:scroll')
+  verificarFooter(): void {
+    if (!this.footer) {
+      return;
+    }
+
+    const footerTop = this.footer.nativeElement.getBoundingClientRect().top;
+
+    const footerEntrouNaTela = footerTop <= window.innerHeight;
+
+    this.mostrarBotaoVoltarAoTopo.set(footerEntrouNaTela);
+  }
+
   voltarAoTopo(): void {
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth',
-  });
-}
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  }
 }
